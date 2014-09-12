faca023588f586a6dea038fef4b9c4a1dc4f8e21
Christopher Styles <christopherstyles@gmail.com>
2014-09-11 21:12:57 -0700

    Add base feature specs

---
+++ spec/github-issues-exporter/web_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+RSpec.describe GithubIssuesExporter::Web, type: :feature do
+  let(:user) do
+    make_user(
+      'login' => 'jimbo',
+      'name' => 'Jimbo Baggins'
+    )
+  end
+
+  describe 'when viewing the Dashboard' do
+    context 'when not signed in' do
+      it 'redirects' do
+        get '/dashboard'
+        expect(last_response).to be_a_redirect
+      end
+
+      it 'redirects to github auth' do
+        get '/dashboard'
+        expect(last_response.location)
+          .to match('https://github.com/login/oauth/authorize')
+      end
+    end
+
+    context 'when signed in' do
+      before(:each) do
+        login_as(user)
+      end
+
+      context 'with no given parameters' do
+        it 'shows the user login' do
+          visit '/dashboard'
+          expect(page).to have_content(user.name)
+        end
+      end
+
+      context 'when a repo is given' do
+        it 'displays open repo issues by default' do
+          visit '/dashboard?repo=testrepo'
+
+          # Currently pulling from fixtures
+          expect(page).to have_content('Found a bug')
+        end
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,25 @@
 require 'coveralls'
 Coveralls.wear!
 
-require 'pry'
-require 'github_issues_exporter'
+require 'rack/test'
+require 'sinatra/auth/github'
+require 'sinatra/auth/github/test/test_helper'
+require 'capybara/rspec'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'coveralls.io')
+
+require File.expand_path '../../lib/github_issues_exporter/web.rb', __FILE__
+
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
+
+RSpec.configure do |config|
+  config.include(Rack::Test::Methods)
+  config.include(Sinatra::Auth::Github::Test::Helper)
+
+  def app
+    GithubIssuesExporter::Web
+  end
+
+  Capybara.app = app
+end
