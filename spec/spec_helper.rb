0cc1898ea6f37c7669e405fd84714cdd66b6cebb
Christopher Styles <christopherstyles@gmail.com>
2014-09-12 17:01:51 -0700

    Move webmock configuration into spec/support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,6 @@ require 'sinatra/auth/github/test/test_helper'
 require 'capybara/rspec'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow_localhost: true, allow: 'coveralls.io')
-
 require File.expand_path '../../lib/github_issues_exporter/web.rb', __FILE__
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -3,3 +3,5 @@ RSpec.configure do |config|
     stub_request(:any, /api.github.com/).to_rack(FakeGitHub)
   end
 end
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'coveralls.io')
