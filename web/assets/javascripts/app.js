0f0739ecbf036d37a5ec88d43a094748f14797cd
Christopher Styles <christopherstyles@gmail.com>
2014-10-10 17:10:06 -0400

    Move form to a side menu

--- web/assets/javascripts/app.js
+++ web/assets/javascripts/app.js
@@ -5,4 +5,18 @@ $(function () {
   $('select[name="labels[]"]').select2({
     placeholder: 'Select a Label'
   });
+
+  var snapper = new Snap({
+    element: document.getElementById('content')
+  });
+
+  $('.toggle-menu').on('click', function(){
+    if( snapper.state().state == 'left' ) {
+        snapper.close();
+    } else {
+        snapper.open('left');
+    }
+  })
+
+  snapper.open('left');
 });
